4532cf6039c2ba2ea730583fefe865d0e63feb5c
Muhammad Abrar <mabrar000999@gmail.com>
2022-12-27 18:27:19 +0500

    Updated JS file

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,7 @@
 const navBar = document.getElementById("navbar");
 const menuBar = document.getElementById("menu_bars");
+
+menuBar.addEventListener("click", () => {
+    menuBar.classList.toggle("fa-times");
+    navBar.classList.toggle("active");
+  });
